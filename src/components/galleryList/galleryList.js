87e63b6abffc089f1d1dd49535bc4af2a7dc7211
Bilek <binienda.a@interia.pl>
2025-03-26 18:03:37 +0100

    .

--- src/components/galleryList/galleryList.js
+++ src/components/galleryList/galleryList.js
@@ -3,7 +3,7 @@ import GalleryItem from "../galleryItem/galleryItem";
 
 export default function GalleryList({ pixabayData, filterTag }) {
   return (
-    <ol className="d-flex flex-wrap justify-content-center list-unstyled mt-2">
+    <ol className="d-flex flex-wrap justify-content-center list-unstyled mt-2 w-100">
       {pixabayData.map((item) => {
         return <GalleryItem key={item.id} item={item} filterTag={filterTag} />;
       })}
